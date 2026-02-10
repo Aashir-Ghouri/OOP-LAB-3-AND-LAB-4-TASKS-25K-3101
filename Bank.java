c0286fa9c06f802071e94f6ad367d10fe25fd91a
Aashir Ghouri <k253101@nu.edu.pk>
2026-02-10 15:41:01 +0500

    Add files via upload

---
+++ Bank.java
@@ -0,0 +1,35 @@
+public class Bank {
+
+    int accNum;
+    String accHolder;
+    double accBal;
+    Bank(){
+        System.out.println("Initialized.");
+    }
+
+    Bank(int PaccNum, String PaccHolder){
+        accNum = PaccNum;
+        accHolder = PaccHolder;
+    }
+
+    Bank(int PaccNum, String PaccHolder, double PaccBal){
+        accNum = PaccNum;
+        accHolder = PaccHolder;
+        accBal = PaccBal;
+    }
+
+    public void display(){
+        System.out.println("Account id : "+this.accNum);
+        System.out.println("Account name : "+this.accHolder);
+        System.out.println("Account balance : "+this.accBal);
+    }
+
+    public static void main(String[] args){
+        Bank acc1 = new Bank(230,"Aashir Ghouri", 1000.00);
+        Bank acc2 = new Bank(130,"Hammad");
+
+        acc1.display();
+        acc2.display();
+
+    }
+}
